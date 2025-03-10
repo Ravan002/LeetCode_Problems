39c4d217ff3895abf24915fa14194fb43e5ea30b
Ravan002 <mammedov.r39@gmail.com>
2025-03-10 20:24:10 +0400

    solve some questions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
 
-        //System.out.println(result);
     }
 }
